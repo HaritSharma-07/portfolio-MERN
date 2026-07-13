8cd07908b75419f5b88e9463d5ae2acf33e49a7e
Harit Sharma <haritsharma0807@gmail.com>
2026-07-13 22:00:41 +0530

    Update App.jsx

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -2,7 +2,7 @@ import Navbar from "./Components/navbar/navbar";
 import Hero from "./Components/hero/hero";
 import About from "./Components/about/about";
 import Skills from "./Components/skills/skills";
-import Projects from "./Components/projects/projects";
+import Projects from "./Components/projects/Projects";
 import Timeline from "./Components/Timeline/timeline";
 import Contact from "./Components/contact/contact";
 import Footer from "./Components/footer/footer";
